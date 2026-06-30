94a7a502980279884ed27bf7cc30d0c1ce15c008
VICTOR HUGO DA SILVA SÁ <victor_sa@estudante.sesisenai.org.br>
2026-06-29 21:55:45 -0300

    02

--- src/question03.js
+++ src/question03.js
@@ -6,12 +6,12 @@ var passeerrado = leia.questionInt("passe errado") * -0.05
 
 if(pontuacao < 50 ){
     console.log("pessima partida")
-}if(pontuacao > 50 && >= 100){
+}else if(pontuacao > 50 && pontuacao2 >= 100){
     console.log("partida ruim")
-}if(pontuacao > 100 && >= 150){
+}else if(pontuacao > 100 && pontuacao2 >= 150){
 console.log("fez o basico")
-}if(pontuacao > 150 && >= 200){
+}else if(pontuacao > 150 && pontuacao2 >= 200){
     console.log("foi bem")
-}if(pontuacao = 200){
+}else if(pontuacao = 200){
     console.log("jogou de mais")
 }
